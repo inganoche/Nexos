94293bc172f59e54b0fd4b9da322c091c547bad0
anoche <desarrollo@palmaceite.com>
2021-03-07 20:35:37 -0500

    Puclicar

--- Frontend/src/app/model/book.ts
+++
@@ -1,8 +0,0 @@
-export class Book {
-    title: string;
-    year: number;
-    pageNumber: number;
-    gender: string;
-    nameEditorial: string;
-    nameAuthor: string;
-}
--- Frontend/src/app/model/task.ts
+++
@@ -1,6 +0,0 @@
-export class Task {
-    userId: number;
-	id: number;
-	title: string;
-	completed: boolean;
-}
